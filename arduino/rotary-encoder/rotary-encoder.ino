726630503357f65a73e186313513c851abdc9f6e
Sebastien Jouhans <sebastien.jouhans@gmail.com>
2019-01-06 18:51:28 +0000

    adding arduino code and wiring diagram

---
+++ arduino/rotary-encoder/rotary-encoder.ino
@@ -0,0 +1,136 @@
+/* 
+ * Created for an arduino leonardo Eth 2
+ */
+
+#include <Ethernet2.h>
+#include <EthernetUdp2.h>
+#include <SPI.h>
+#include <OSCBundle.h>
+
+EthernetUDP Udp;
+
+#define encoder0PinA 2
+#define encoder0PinB 3
+volatile  int encoder0Pos = 4323;
+int encoderPosOld = 0;
+int sensitivity = 2;
+int vidLength = (1024 * sensitivity); 
+float scrub = 0;
+int encoderTick = 0;
+boolean tick = false;
+
+
+
+
+//the Arduino's IP
+IPAddress ip(169, 254, 227, 190);
+//destination IP
+IPAddress outIp(169, 254, 227, 11);
+const unsigned int outPort = 57121;
+
+byte mac[] = {
+  0x90, 0xA2, 0xDA, 0x10, 0xEB, 0xE4
+}; // you can find this written on the board of some Arduino Ethernets or shields
+
+void setup() {
+  pinMode(encoder0PinA, INPUT);
+  pinMode(encoder0PinB, INPUT);
+  // encoder pin on interrupt 0 (pin 2)
+  attachInterrupt(0, doEncoderA, CHANGE);
+  // encoder pin on interrupt 1 (pin 3)
+  attachInterrupt(1, doEncoderB, CHANGE);
+
+  Ethernet.begin(mac, ip);
+  Udp.begin(8888);
+  Serial.begin (115200);
+}
+
+void loop() {
+  rangeCheck();
+  oscSend();
+}
+
+void doEncoderA() {
+
+  // look for a low-to-high on channel A
+  if (digitalRead(encoder0PinA) == HIGH) {
+    // check channel B to see which way encoder is turning
+    if (digitalRead(encoder0PinB) == LOW) {
+      encoder0Pos = encoder0Pos + 1;
+    }         // CW
+    else {
+      encoder0Pos = encoder0Pos - 1;  // CCW
+    }
+  }
+  else   // must be a high-to-low edge on channel A
+  {
+    // check channel B to see which way encoder is turning
+    if (digitalRead(encoder0PinB) == HIGH) {
+      encoder0Pos = encoder0Pos - 1;
+    }         // CW
+    else {
+      encoder0Pos = encoder0Pos + 1;   // CCW
+    }
+  }
+
+  tick = true;
+  //Serial.println(encoder0Pos);
+}
+
+void doEncoderB() {
+
+  // look for a low-to-high on channel B
+  if (digitalRead(encoder0PinB) == HIGH) {
+    // check channel A to see which way encoder is turning
+    if (digitalRead(encoder0PinA) == HIGH) {
+      encoder0Pos = encoder0Pos + 1;
+    }       // CW
+    else {
+      encoder0Pos = encoder0Pos - 1;    // CCW
+    }
+  }
+  // Look for a high-to-low on channel B
+  else {
+    // check channel B to see which way encoder is turning
+    if (digitalRead(encoder0PinA) == LOW) {
+      encoder0Pos = encoder0Pos + 1;
+    }        // CW
+    else {
+      encoder0Pos = encoder0Pos - 1;   // CCW
+    }
+  }
+
+  tick = true;
+  //Serial.println(encoder0Pos);
+}
+
+void oscSend() {
+  if (tick == true) {
+    //declare the bundle
+    OSCBundle bndl;
+
+    //BOSCBundle's add' returns the OSCMessage so the message's 'add' can be composed together
+    bndl.add("/scrub").add((float)scrub);
+    Udp.beginPacket(outIp, outPort);
+    bndl.send(Udp); // send the bytes to the SLIP stream
+    Udp.endPacket(); // mark the end of the OSC Packet
+    bndl.empty(); // empty the bundle to free room for a new one
+    tick = false;
+    Serial.println(encoder0Pos);
+    Serial.println(scrub);
+    Serial.println(scrub * 1000);
+    Serial.println(encoderTick);
+    Serial.println("------");
+  }
+}
+
+void rangeCheck() {
+  if (encoder0Pos < 0) {
+    scrub = 1 + (float(encoder0Pos % vidLength) / float (vidLength)) ;
+  }
+  else {
+    scrub = float(encoder0Pos % vidLength) / float (vidLength);
+  }
+  
+  encoderTick = int((vidLength * scrub));
+}
--- electron-app/README.md
+++ electron-app/README.md
@@ -1,6 +1,9 @@
-## Overview
+
+# Overview
 
 Electron app implementing OSC.js. This current sample work with a [Rotary Encoder](https://coolcomponents.co.uk/products/rotary-encoder-1024-p-r-quadrature) which is connected to an Arduino board. The arduino board sends data coming from the rotary encoder to the Electron app over OSC.
+
+# Electron
 
 ## Dependencies
 
@@ -19,7 +22,7 @@ npm install
 * change directory
 
 ```shell
-cd electron-osc-boilerplate
+cd electron-osc-boilerplate/electron
 ```
 
 * install dependencies
@@ -39,10 +42,6 @@ Read the osc documentation about installation with Electron. https://github.com/
 
 **make sure that you are using python 2 to make the build**
 
-## Running the Example
-
-1. Run <code>npm run electron-app</code>
-
 ## Adding new mpm modules
 
 Everytime a new npm module is added you need to run this command to rebuild electron
@@ -56,3 +55,18 @@ npm run electron-app
 ## Computer IP
 
 The ethernet port where the device sending OSC data is connected needs to be set to 169, 254, 227, 11.
+
+# Arduino
+
+## Prerequisite libraries
+
+* [Arduino 1.8.5](https://www.arduino.cc/en/Main/Software)
+* 2 libraries need to be installed
+  * [Ethernet2](https://www.arduinolibraries.info/libraries/ethernet2)
+  * [OSCBundle](https://github.com/CNMAT/OSC)
+
+## Wiring 
+
+This is wiring digram that i used for this sample but any other sensor could be used.
+
+![Alt text](assets/Wiring_diagram.png?raw=true "Wiring diagram")
